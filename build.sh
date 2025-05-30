ae20e7691315cdbf46a7275a6e85728b15d61c53
Tommaso Visconti <tommaso.visconti@gmail.com>
2025-05-31 00:32:23 +0200

    update build command

--- build.sh
+++ build.sh
@@ -8,4 +8,5 @@ west init -l config
 west update
 west zephyr-export
 
-west -v build -s zmk/app -b nice_nano_v2 -S zmk-usb-logging -- -DSHIELD=handwired_68 -DZMK_CONFIG=/workspace/zmk-config/config
+west -v build -s zmk/app -b nice_nano_v2 -S studio-rpc-usb-uart -S cdc-acm-console -- -DSHIELD=handwired_68 -DZMK_CONFIG=/workspace/zmk-config/config
+
